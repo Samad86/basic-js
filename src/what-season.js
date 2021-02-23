88401f436ff6a8aeec07c3d3dc980f8bec0a3f4c
samad86 <mads198644@gmail.com>
2021-02-23 18:06:50 +0300

    feat: add solution what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,39 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+//1й способ:
+
+/*
+module.exports = function getSeason(date) {
+  if (date === undefined) return 'Unable to determine the time of year!';
+  if (Object.prototype.toString.call(date) !== '[object Date]') throw new Error('THROWN');
+
+  if ((date.getMonth() >= 0 && date.getMonth() <= 1) || date.getMonth() == 11) return 'winter';
+  if (date.getMonth() >= 2 && date.getMonth() <= 4) return 'spring';
+  if (date.getMonth() >= 5 && date.getMonth() <= 7) return 'summer';
+  if (date.getMonth() >= 8 && date.getMonth() <= 10) return 'autumn';
+};
+*/
+
+//2й способ:
+
+const SEASONS = [
+  'winter',
+  'winter',
+  'spring',
+  'spring',
+  'spring',
+  'summer',
+  'summer',
+  'summer',
+  'autumn',
+  'autumn',
+  'autumn',
+  'winter',
+];
+
+module.exports = function getSeason(date) {
+  if (date === undefined) return 'Unable to determine the time of year!';
+  if (Object.prototype.toString.call(date) !== '[object Date]') throw new Error('THROWN');
+
+  return SEASONS[date.getMonth()];
 };
